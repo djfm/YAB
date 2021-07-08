8bef5b12a7a95ac7baf65102a01d6d5e8d168a3f
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-07-08 17:01:30 +0200

    removed src/exampleSourceFiles, no longer useful

--- src/exampleSourceFiles/index.ts
+++
@@ -1,9 +0,0 @@
-import {
-  x,
-  y,
-  z,
-} from './moduleB';
-
-import lambda from './moduleA';
-
-console.log(x + y + z, lambda);
--- src/exampleSourceFiles/moduleB.ts
+++
@@ -1,7 +0,0 @@
-export const x = 4;
-
-export const y = 5;
-
-export const z = 48;
-
-export default z;
